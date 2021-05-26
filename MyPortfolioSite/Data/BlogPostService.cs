5c5b6de3bb06a99ba5e7c0cad2dfd826658f950c
Matthew Klawitter <mtklaw153@gmail.com>
2021-05-25 22:50:30 -0400

    Add basic CRUD implementation for BlogPosts and Tag support

--- MyPortfolioSite/Data/BlogPostService.cs
+++ MyPortfolioSite/Data/BlogPostService.cs
@@ -16,10 +16,20 @@ namespace MyPortfolioSite.Data
         }
 
         // Insert new BlogPost
-        public async Task<bool> InsertBlogPostAsync(BlogPost blogPost)
+        public async Task<bool> InsertBlogPostAsync(BlogPost blogPost, List<string> tagIds)
         {
             using (var context = _contextFactory.CreateDbContext())
             {
+                List<int> ids = new List<int>();
+                foreach (var tag in tagIds)
+                {
+                    ids.Add(int.Parse(tag));
+                }
+
+                var tags = await context.Tags.Where(t => ids.Contains(t.Id)).ToListAsync();
+
+                blogPost.Tags = tags;
+
                 await context.BlogPosts.AddAsync(blogPost);
                 await context.SaveChangesAsync();
                 return true;
@@ -31,7 +41,9 @@ namespace MyPortfolioSite.Data
         {
             using (var context = _contextFactory.CreateDbContext())
             {
-                return await context.BlogPosts.ToListAsync();
+                return await context.BlogPosts
+                    .Include(blog => blog.Tags)
+                    .ToListAsync();
             }
         }
 
@@ -40,17 +52,44 @@ namespace MyPortfolioSite.Data
         {
             using (var context = _contextFactory.CreateDbContext())
             {
-                BlogPost blogPost = await context.BlogPosts.FirstOrDefaultAsync(c => c.Id.Equals(Id));
+                BlogPost blogPost = await context.BlogPosts
+                    .Include(blog => blog.Tags)
+                    .FirstOrDefaultAsync(c => c.Id.Equals(Id));
                 return blogPost;
             }
         }
 
         // Update BlogPost
-        public async Task<bool> UpdateBlogPostAsync(BlogPost blogPost)
+        public async Task<bool> UpdateBlogPostAsync(BlogPost updatedPost, List<string> tagIds)
         {
             using (var context = _contextFactory.CreateDbContext())
             {
-                context.BlogPosts.Update(blogPost);
+                BlogPost blogPost = await context.BlogPosts
+                    .Include(blog => blog.Tags)
+                    .FirstOrDefaultAsync(c => c.Id.Equals(updatedPost.Id));
+
+                // Convert the posted Tag Id's from string to int for convenience
+                List<int> ids = new List<int>();
+                foreach (var tag in tagIds)
+                {
+                    ids.Add(int.Parse(tag));
+                }
+
+                // Tags that should exist in the updated Post.Tags
+                var newTags = await context.Tags.Where(t => ids.Contains(t.Id)).ToListAsync();
+
+                blogPost.Title = updatedPost.Title;
+                blogPost.Desc = updatedPost.Desc;
+                blogPost.Author = updatedPost.Author;
+                blogPost.Content = updatedPost.Content;
+                blogPost.IsPublic = updatedPost.IsPublic;
+                blogPost.DateUpdated = DateTime.UtcNow;
+
+                // Update list of Tags
+                blogPost.Tags.Clear();
+                blogPost.Tags = newTags;
+
+                // Save changes
                 await context.SaveChangesAsync();
                 return true;
             }
