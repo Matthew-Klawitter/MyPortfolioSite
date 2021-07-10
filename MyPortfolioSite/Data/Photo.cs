e77bde37b68fafdeb919159b1b7e18c527cdc7ed
Matthew Klawitter <mtklaw153@gmail.com>
2021-07-10 18:15:39 -0400

    Added basic photo upload support

---
+++ MyPortfolioSite/Data/Photo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolioSite.Data
+{
+    public class Photo
+    {
+        public int Id;
+        public string FileName { get; set; }
+        public string Desc { get; set; }
+        public string Path { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+
+        public Photo()
+        {
+            this.DateCreated = DateTime.UtcNow;
+            this.DateUpdated = DateTime.UtcNow;
+        }
+
+        public bool UpdateDate()
+        {
+            this.DateUpdated = DateTime.UtcNow;
+            return true;
+        }
+    }
+}
